a65c6420917151d473052384845e86b7177c5bba
Shahriar Dhruvo <shahriardhruvo119@gmail.com>
2025-02-14 00:01:32 +0600

    Add btop config; Update nvim, ghostty & wezterm config for linux;

--- linux/.wezterm.lua
+++ linux/.wezterm.lua
@@ -7,11 +7,9 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
-config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
-config.font_size = 16
+config.font = wezterm.font("JetBrains Mono", { weight = "Medium" })
+config.font_size = 14
 config.color_scheme = "Ef-Night"
-config.window_background_opacity = 0.85
--- config.macos_window_background_blur = 10
 
 -- and finally, return the configuration to wezterm
 return config
